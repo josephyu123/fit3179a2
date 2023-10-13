301f3179d5373cdebba1dd57b385e8f6ed59fd0e
Joseph Yu <jyuu0052@student.monash.edu>
2023-10-13 21:14:11 +1100

    map 1 finish

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-config = 'weather-data.json';
+vegaEmbed("#map_1", 'minimum-temperature.json').catch(console.error);
+vegaEmbed("#map_2", 'maximum-temperature.json').catch(console.error);
+document.getElementById('map_2').classList.add('hidden');
 
-vegaEmbed("#map_1", config).then(function(result) {
-    
-}).catch(console.error);
+document.getElementById("mapSelector").addEventListener("change", function() {
+    var selectedFile = this.value;
+
+    if (selectedFile === 'minimum-temperature.json') {
+        document.getElementById('map_1').classList.remove('hidden');
+        document.getElementById('map_2').classList.add('hidden');
+    } else {
+        document.getElementById('map_1').classList.add('hidden');
+        document.getElementById('map_2').classList.remove('hidden');
+    }
+});
