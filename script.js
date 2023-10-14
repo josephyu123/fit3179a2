7e9d758542b812f02bb39f1d27f446cb998d7bbb
Joseph Yu <jyuu0052@student.monash.edu>
2023-10-15 03:48:00 +1100

    second map complete

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 vegaEmbed("#map_1", 'minimum-temperature.json').catch(console.error);
 vegaEmbed("#map_2", 'maximum-temperature.json').catch(console.error);
+vegaEmbed("#map_3", 'avg-temperature-2020-2039.json').catch(console.error);
+vegaEmbed("#map_4", 'avg-temperature-2040-2059.json').catch(console.error);
+vegaEmbed("#map_5", 'avg-temperature-2060-2079.json').catch(console.error);
+vegaEmbed("#map_6", 'avg-temperature-2080-2099.json').catch(console.error);
+
+vegaEmbed("#bar_1", 'min-temperature.json').catch(console.error);
+
 document.getElementById('map_2').classList.add('hidden');
+document.getElementById('map_4').classList.add('hidden');
+document.getElementById('map_5').classList.add('hidden');
+document.getElementById('map_6').classList.add('hidden');
 
 document.getElementById("mapSelector").addEventListener("change", function() {
     var selectedFile = this.value;
@@ -11,5 +21,31 @@ document.getElementById("mapSelector").addEventListener("change", function() {
     } else {
         document.getElementById('map_1').classList.add('hidden');
         document.getElementById('map_2').classList.remove('hidden');
+    }
+});
+
+document.getElementById("yearSelector").addEventListener("change", function() {
+    var selectedFile = this.value
+
+    if (selectedFile === 'avg-temperature-2020-2039.json') {
+        document.getElementById('map_3').classList.remove('hidden');
+        document.getElementById('map_4').classList.add('hidden');
+        document.getElementById('map_5').classList.add('hidden');
+        document.getElementById('map_6').classList.add('hidden');
+    } else if (selectedFile === 'avg-temperature-2040-2059.json') {
+        document.getElementById('map_3').classList.add('hidden');
+        document.getElementById('map_4').classList.remove('hidden');
+        document.getElementById('map_5').classList.add('hidden');
+        document.getElementById('map_6').classList.add('hidden');
+    } else if (selectedFile === 'avg-temperature-2060-2079.json') {
+        document.getElementById('map_3').classList.add('hidden');
+        document.getElementById('map_4').classList.add('hidden');
+        document.getElementById('map_5').classList.remove('hidden');
+        document.getElementById('map_6').classList.add('hidden');
+    } else {
+        document.getElementById('map_3').classList.add('hidden');
+        document.getElementById('map_4').classList.add('hidden');
+        document.getElementById('map_5').classList.add('hidden');
+        document.getElementById('map_6').classList.remove('hidden');
     }
 });
